5eb9c970e6b27a0745697069612a39a189c4737e
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-03-21 16:17:43 -0500

    A ver si corre

--- prep/prep_squad.py
+++ prep/prep_squad.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy.random as npr
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+# from __future__ import absolute_import
+# from __future__ import division
+# from __future__ import print_function
 import numpy as np
 from collections import Counter
 from nltk.tokenize import TweetTokenizer
