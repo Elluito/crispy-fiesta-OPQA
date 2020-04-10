57033dd489a5f15507b5eb1c80d57a68764ae1dd
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-04-10 12:51:44 -0500

    CAMBIOS

--- prueba_gpu.py
+++ prueba_gpu.py
@@ -5,7 +5,7 @@ a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
 b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
 c = tf.matmul(a, b)
 
-print(c)
+print(c.device)
 
 # Place tensors on the CPU
 with tf.device('/CPU:0'):
@@ -13,4 +13,4 @@ with tf.device('/CPU:0'):
   b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
 
 c = tf.matmul(a, b)
-print(c)
+print(c.device)
